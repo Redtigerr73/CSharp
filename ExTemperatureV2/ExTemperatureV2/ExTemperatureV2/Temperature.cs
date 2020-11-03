79daf11de6ee016e412ae13de3ffbc8944fc568d
Mohamad HAJI <PSR11287@students.ephec.be>
2020-11-03 22:31:13 +0100

    ok

---
+++ ExTemperatureV2/ExTemperatureV2/ExTemperatureV2/Temperature.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTemperatureV2
+{
+    class Temperature
+    {
+        public double tempEnCel { get; set; }
+        public double tempEnFahr { get; set; }
+        public char unite { get; set; }
+
+        public Temperature(double temp, char unite)
+        {
+            if (unite == 'C')
+            tempEnCel = temp;
+            if (unite == 'F')
+            tempEnFahr = temp;
+            this.unite = unite;
+        }
+
+        public double EnCel()
+        {
+            if (unite == 'F')
+                return tempEnFahr = (tempEnCel - 32) * 5 / 9;
+            else
+                return tempEnCel;
+        }
+
+        public double EnFahr()
+        {
+            if (unite == 'C')
+                return tempEnCel = tempEnFahr * 9 / 5 + 32;
+            else
+                return tempEnFahr;
+        }
+
+        public bool Chauffage()
+        {
+            if (EnCel() < 15)
+                return true;
+            else return false;
+        }
+
+
+      
+
+    }
+}
---
+++ ExTemperatureV2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTemperatureV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature(40, 'C');
+            Console.WriteLine(temp);
+            Console.WriteLine($"La temperature est de {temp.tempEnCel} il faut ouvrir le chauffage est : {temp.Chauffage()}");
+            temp.tempEnCel = 14;
+            Console.WriteLine($"La temperature est de {temp.tempEnCel} il faut ouvrir le chauffage est : {temp.Chauffage()}");
+
+        }
+    }
+}
