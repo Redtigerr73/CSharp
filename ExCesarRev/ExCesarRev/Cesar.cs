0fa5c22e20c7ecd6837961b1de23ad6264b6bd88
Redtigerr73 <64441143+Redtigerr73@users.noreply.github.com>
2020-11-03 15:32:22 +0100

    test3

---
+++ ExCesarRev/ExCesarRev/Cesar.cs
@@ -0,0 +1,42 @@
+﻿namespace ExCesarRev
+{
+    class Cesar
+    {
+        private int _clef;
+
+        public Cesar(int clef)
+        {
+            _clef = clef;
+        }
+
+        public string Chiffrer(string phrase)
+        {
+            char[] array = phrase.ToCharArray();
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                int code = phrase[i] + _clef;
+                if (code > 'z')
+                    code -= (26 - _clef);
+                array[i] = (char)(code);
+            }
+            return new string(array);
+        }
+
+        public string Dechiffrer(string phraseADechiffrer)
+        {
+            char[] array = phraseADechiffrer.ToCharArray();
+
+            for (int i = 0; i < phraseADechiffrer.Length; i++)
+            {
+                int code = phraseADechiffrer[i] - _clef;
+                if (code < 'a')
+                    code += (26 - _clef);
+                array[i] = (char)(code);
+            }
+            return new string(array);
+        }
+
+
+    }
+}
---
+++ ExCesarRev/ExCesarRev/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExCesarRev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cesar message = new Cesar(3);
+            string msgChiffre = message.Chiffrer("toto");
+            Console.WriteLine("Le message a chiffrer est : toto");
+            Console.WriteLine($"Le message chiffré est : {msgChiffre}");
+            Console.WriteLine($"Le message déchiffré est : {message.Dechiffrer(msgChiffre)}");
+        }
+    }
+}
