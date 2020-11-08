5b6fa5c33a238ad5a213b3889bfa4bb53977b251
Redtigerr73 <64441143+Redtigerr73@users.noreply.github.com>
2020-11-08 22:20:04 +0100

    ok

---
+++ PersonneBelgeRev/PersonneBelgeRev/Belge.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonneBelgeRev
+{
+    class Belge : Personne
+    {
+        # region Champ
+        private string _communaute;
+
+        
+        #endregion
+
+        #region Propriété
+        public string Communaute
+        {
+            get { return _communaute; }
+            set { _communaute = value; }
+        }
+        #endregion
+
+        #region Constructeur
+        public Belge(string Nom, string Prenom, string com) : base(Nom, Prenom)
+        {
+            Communaute = com;
+        }
+        #endregion
+
+        #region Méthode
+        public new void Afficher()
+        {
+            Console.Write($"Nom : {Nom} et Prénom : { Prenom}");
+            Console.Write($" + {Communaute}\n");
+        }
+        #endregion
+
+    }
+}
---
+++ PersonneBelgeRev/PersonneBelgeRev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonneBelgeRev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne("Toto", "Tata");
+            Personne p2 = new Personne("Titi", "Tutu");
+
+            Belge p3 = new Belge("Jean", "Frite", "Bruxellois");
+            p1.Afficher();
+            p3.Afficher();
+            p3.Afficher();
+            
+        }
+    }
+}
